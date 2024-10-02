12613d8c06c042290c12c9266204a56640690add
thainatuanne <thaina-tuanne@hotmail.com>
2024-10-02 16:02:52 -0300

    add commit, index.js, validation.js, messages.js, users.js

---
+++ aplicacaoListaDeRecados/src/routes/messages.js
@@ -0,0 +1,99 @@
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
+import { users } from "./users.js";
+import { validateEmail } from "./validation.js";
+
+export const messagesRouter = express.Router();
+
+let messages = [];
+
+messagesRouter.post("/", validateEmail, (req, res) => {
+  const { email, title, description } = req.body;
+
+  if (!title || !description) {
+    return res.status(400).json({
+      message: "Por favor, forneça título e descrição.",
+    });
+  }
+
+  const user = users.find((u) => u.email === email);
+  if (!user) {
+    return res.status(404).json({
+      message: "Usuário não encontrado.",
+    });
+  }
+
+  const newMessage = {
+    id: uuidv4(),
+    title,
+    description,
+    userId: user.id,
+  };
+  messages.push(newMessage);
+
+  return res.status(201).json({
+    message: "Mensagem criada com sucesso!",
+    data: newMessage,
+  });
+});
+
+messagesRouter.get("/:email", (req, res) => {
+  const { email } = req.params;
+  const user = users.find((u) => u.email === email);
+
+  if (!user) {
+    return res.status(404).json({
+      message: "Usuário não encontrado.",
+    });
+  }
+
+  const userMessages = messages.filter((m) => m.userId === user.id);
+
+  return res.status(200).json({
+    message: "Seja bem-vinde!",
+    data: userMessages,
+  });
+});
+
+messagesRouter.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+
+  const message = messages.find((m) => m.id === id);
+  if (!message) {
+    return res.status(404).json({
+      message: "Mensagem não encontrada.",
+    });
+  }
+
+  if (!title || !description) {
+    return res.status(400).json({
+      message: "Por favor, forneça título e descrição.",
+    });
+  }
+
+  message.title = title;
+  message.description = description;
+
+  return res.status(200).json({
+    message: "Mensagem atualizada com sucesso!",
+    data: message,
+  });
+});
+
+messagesRouter.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const messageIndex = messages.findIndex((m) => m.id === id);
+
+  if (messageIndex === -1) {
+    return res.status(404).json({
+      message: "Mensagem não encontrada.",
+    });
+  }
+
+  messages.splice(messageIndex, 1);
+
+  return res.status(200).json({
+    message: "Mensagem apagada com sucesso",
+  });
+});
---
+++ aplicacaoListaDeRecados/src/routes/users.js
@@ -0,0 +1,72 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
+import { validateEmail } from "./validation.js";
+
+export const usersRouter = express.Router();
+
+let users = [];
+
+const isEmailRegistered = (email) => users.some((user) => user.email === email);
+
+usersRouter.post("/signup", validateEmail, async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name) {
+    return res.status(400).json({
+      message: "Por favor, verifique se passou o nome.",
+    });
+  }
+  if (!password) {
+    return res.status(400).json({
+      message: "Por favor, verifique se passou a senha.",
+    });
+  }
+
+  if (isEmailRegistered(email)) {
+    return res.status(400).json({
+      message: "Email já cadastrado, insira outro.",
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = {
+    id: uuidv4(),
+    name,
+    email,
+    password: hashedPassword,
+  };
+  users.push(newUser);
+
+  return res.status(201).json({
+    message: `Seja bem-vindo ${name}! Pessoa usuária registrada com sucesso!`,
+  });
+});
+
+usersRouter.post("/login", validateEmail, async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!password) {
+    return res.status(400).json({
+      message: "Insira uma senha válida.",
+    });
+  }
+
+  const user = users.find((u) => u.email === email);
+  if (!user) {
+    return res.status(404).json({
+      message: "Email não encontrado no sistema, verifique ou crie uma conta.",
+    });
+  }
+
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) {
+    return res.status(400).json({
+      message: "Senha incorreta.",
+    });
+  }
+
+  return res.status(200).json({
+    message: `Seja bem-vindo ${user.name}! Pessoa usuária logada com sucesso!`,
+  });
+});
